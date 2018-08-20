fbeb18a57efc09c0fe90f2da1015f5288e3cc1b5
RoyPy <bogush.vano@gmail.com>
2018-08-20 14:24:03 +0700

    Implement winning

--- src/index.js
+++ src/index.js
@@ -16,16 +16,21 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      winner: null,
     }
   }
 
   handleClick(i) {
+    if (this.state.winner) {
+      return;
+    }
     const squares = this.state.squares.slice();
     if (squares[i] === null) {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
         xIsNext: !this.state.xIsNext,
+        winner: calculateWinner(squares),
       });
     }
   }
@@ -38,7 +43,9 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const status = this.state.winner ?
+      'Winner: ' + this.state.winner :
+      'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -85,3 +92,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
